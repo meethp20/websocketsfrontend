e52f6e39ec0d449fa63c657c6a8dbe7d69c7d41b
Meeth Patel <meeth2060@gmail.com>
2025-07-03 23:14:33 +0530

    made the basicest chat app acc to me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
-
-
-
+"use client"
+import { useState,useEffect } from "react"
 export default function Home(){
-
+const [socket,setSocket] = useState<WebSocket|null>(null)
+const [input,setInput] = useState("")
+const [messeages,setMesseges] = useState<string>("");
+useEffect(()=>{
+  const ws= new WebSocket('ws://localhost:3000')
+  ws.onopen=()=>{
+    console.log("connected")
+    setSocket(ws)
+  }
+  ws.onmessage=(message)=>{
+    console.log("we've a message for you")
+    setMesseges(message.data)
+  }
+},[])
+if(!socket){
   return(
     <div>
+      loading chats
+    </div>
+  )
+}
+  return( 
+    <div>
+      <input value={input} onChange={(e)=>setInput(e.target.value)}/>
+      <button onClick={()=>setMesseges(input)}>Send</button>
+           {messeages && (
+            <p>{messeages}</p>
+           )}
 
     </div>
   )
